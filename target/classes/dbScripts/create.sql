f95c8c0d7049eec6359a3e858232600c865ade7b
artem <yaroslav.gorbatyuk.2010@gmail.com>
2024-05-03 11:48:35 +0300

    new commit

---
+++ target/classes/dbScripts/create.sql
@@ -0,0 +1,7 @@
+create table person(
+                       id serial primary key ,
+                       name varchar,
+                       age int,
+                       email varchar
+);
+
---
+++ target/classes/dbScripts/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.person (id, name, age, email) VALUES (5, 'Artemiy', 19, 'a4@gmail.com');
+INSERT INTO public.person (id, name, age, email) VALUES (2, 'Abbas', 39, 'abbas@gmail.com');
+INSERT INTO public.person (id, name, age, email) VALUES (3, 'Grisha', 21, 'asdsad@gmail.com');
+INSERT INTO public.person (id, name, age, email) VALUES (8, 'Vasya', 19, 'asdasd@gmail.com');
